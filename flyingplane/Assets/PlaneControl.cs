a8251385a2b4c4a1dd85d02f5e8de8e9278a4501
jwang246 <jonathanwang@jhu.edu>
2018-04-17 23:42:08 -0400

    imported plane model, basic 2d movement and rotation

---
+++ flyingplane/Assets/PlaneControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = 0.0f;// Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * 15 * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * 15 * Time.deltaTime);
+
+        }
+    }
+}
