cea7c2ec9ef0abcd258723ee6dfe48ecca570065
jonathan <jonathanwang@jhu.edu>
2018-04-25 17:55:10 -0400

    fixed hololens integration and airplane controls

--- flying-plane/Assets/PlaneControl.cs
+++ flying-plane/Assets/PlaneControl.cs
@@ -1,82 +1,105 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloLensXboxController;
+using UnityEngine.UI;
 
 public class PlaneControl : MonoBehaviour
 {
 
-    // Use this for initialization
-
-    public float speed;
     private Rigidbody rb;
+
+    private ControllerInput controllerInput;
+
+    public float RotateAroundYSpeed = 2.0f;
+    public float RotateAroundXSpeed = 2.0f;
+    public float RotateAroundZSpeed = 2.0f;
+    public float accelerationSpeed = .01f;
+    public float topSpeed = 1.0f;
+    public float minSpeed = 0.25f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        controllerInput = new ControllerInput(0, 0.19f);
     }
 
     void FixedUpdate()
     {
-        /*float moveHorizontal = 0.0f;// Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);*/
 
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddRelativeForce(Vector3.forward * speed);
+            rb.transform.Translate(Vector3.forward * accelerationSpeed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddRelativeForce(Vector3.back * speed);
+            rb.transform.Translate(Vector3.forward * accelerationSpeed);
         }
 
-        /*if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddRelativeForce(Vector3.left * 0.25f * speed);
-        }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddRelativeForce(Vector3.right * 0.25f * speed);
-        }*/
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.down * 45 * Time.deltaTime);
+        controllerInput.Update();
 
+         if (Input.GetKey(KeyCode.A))
+         {
+             transform.Rotate(Vector3.down * 45 * Time.deltaTime);
+
+         }
+
+         if (Input.GetKey(KeyCode.D))
+         {
+             transform.Rotate(Vector3.up * 45 * Time.deltaTime);
+         }
+
+         if (Input.GetKey(KeyCode.UpArrow))
+         {
+             transform.Rotate(Vector3.left * 45 * Time.deltaTime);
+         }
+
+         if (Input.GetKey(KeyCode.DownArrow))
+         {
+             transform.Rotate(Vector3.right * 45 * Time.deltaTime);
+         }
+
+         if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             transform.Rotate(Vector3.forward * 45 * Time.deltaTime);
+         }
+
+         if (Input.GetKey(KeyCode.RightArrow))
+         {
+             transform.Rotate(Vector3.back * 45 * Time.deltaTime);
+         }
+
+        translateRotateScale();
+        translateForward();
+    }
+
+    private void translateRotateScale()
+    {
+
+        float rotateAroundY = RotateAroundYSpeed * controllerInput.GetAxisLeftThumbstickX();
+        float rotateAroundX = RotateAroundXSpeed * controllerInput.GetAxisLeftThumbstickY();
+        float rotateAroundZ = RotateAroundZSpeed * controllerInput.GetAxisRightThumbstickX();
+        this.transform.Rotate(rotateAroundX, rotateAroundY, -rotateAroundZ);
+    }
+
+    private void translateForward()
+    {
+        rb.transform.Translate(Vector3.forward * (controllerInput.GetAxisRightTrigger() - controllerInput.GetAxisLeftTrigger()) * accelerationSpeed);
+
+        if (rb.velocity.magnitude > topSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * topSpeed;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (rb.velocity.magnitude < minSpeed)
         {
-            transform.Rotate(Vector3.up * 45 * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Rotate(Vector3.left * 45 * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Rotate(Vector3.right * 45 * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(Vector3.forward * 45 * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(Vector3.back * 45 * Time.deltaTime);
+            rb.velocity = rb.velocity.normalized * minSpeed;
         }
     }
 }
