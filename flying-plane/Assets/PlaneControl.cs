e3716ff5d8d2c20c54a996a6a44d12f3ea8ae683
jonathan <jonathanwang@jhu.edu>
2018-04-29 18:04:15 -0400

    trying to fix movement

--- flying-plane/Assets/PlaneControl.cs
+++ flying-plane/Assets/PlaneControl.cs
@@ -18,42 +18,62 @@ public class PlaneControl : MonoBehaviour
     public float minSpeed = 0.25f;
 
     public GameObject WaterDrop;
+    public float speed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         controllerInput = new ControllerInput(0, 0.19f);
+        speed = 0;
     }
 
     void FixedUpdate()
     {
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.transform.Translate(Vector3.forward * accelerationSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.transform.Translate(Vector3.forward * accelerationSpeed);
-        }
-
+        //rb.transform.Translate(Vector3.forward * speed);
 
     }
     
     void Update()
     {
         controllerInput.Update();
+        if (Input.GetKey(KeyCode.W))
+        {
+            //rb.transform.Translate(Vector3.forward * accelerationSpeed);
+            rb.velocity += Vector3.forward * accelerationSpeed;
+            //if (transform.rotation.eulerAngles.z > 180)
+            //{
+            //    rb.transform.Rotate(Vector3.right * (Mathf.Sin((-270 + transform.rotation.eulerAngles.z) * Mathf.PI / 180)));
+            //    rb.transform.Rotate(Vector3.up * (Mathf.Sin((360 - transform.rotation.eulerAngles.z) * Mathf.PI / 180)));
+            //}
 
-         if (Input.GetKey(KeyCode.A))
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.transform.Translate(Vector3.back * accelerationSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
          {
-             transform.Rotate(Vector3.down * 45 * Time.deltaTime);
+            //transform.Rotate(Vector3.down * 45 * Time.deltaTime);
+            // if (transform.rotation.z < 0.2f)
+            //{
+            //    Debug.Log("z value is: " + transform.rotation.eulerAngles.z);
+            //    transform.Rotate(Vector3.forward * 45 * Time.deltaTime);
+            //}
+            rb.AddForce(Vector3.left * accelerationSpeed, ForceMode.Impulse);
 
          }
 
          if (Input.GetKey(KeyCode.D))
          {
-             transform.Rotate(Vector3.up * 45 * Time.deltaTime);
+             //transform.Rotate(Vector3.up * 45 * Time.deltaTime);
+            if (transform.rotation.z > -0.2f)
+            {
+                Debug.Log("z value is: " + transform.rotation.eulerAngles.z);
+                transform.Rotate(Vector3.back * 45 * Time.deltaTime);
+            }
          }
 
          if (Input.GetKey(KeyCode.UpArrow))
