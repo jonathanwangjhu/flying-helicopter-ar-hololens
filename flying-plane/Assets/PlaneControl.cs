c1f397bb7a7b337584d0010191890ce8adefdb62
unknown <pratyush.trivedi72@gmail.com>
2018-05-01 11:57:01 -0400

    helicopter movement`

--- flying-plane/Assets/PlaneControl.cs
+++ flying-plane/Assets/PlaneControl.cs
@@ -19,6 +19,13 @@ public class PlaneControl : MonoBehaviour
     public float topSpeed = 1.0f;
     public float minSpeed = 0.25f;
     public float speed = 0;
+    public float accel = .5f;
+    public Vector3 forceup;
+    public Vector3 forcedown;
+    public Vector3 forceforward;
+    public Vector3 forceback;
+    public Vector3 forceleft;
+    public Vector3 forceright;
 
     public GameObject WaterDrop;
 
@@ -30,14 +37,68 @@ public class PlaneControl : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         controllerInput = new ControllerInput(0, 0.19f);
+
+        //(PT) set these here so they aren't affected by rotation later
+        forceup = Vector3.up;
+        forcedown = Vector3.down;
+        forceforward = Vector3.forward;
+        forceback = Vector3.back;
+        forceright = Vector3.right;
+        forceleft = Vector3.left;
     }
 
     void FixedUpdate()
     {
         if (canControl)
         {
-            //rb.transform.Translate(Vector3.forward * speed);
+            //move up
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(forceup * 2, ForceMode.Acceleration);
+            }
 
+            //move forward, backward, left, and right
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.AddForce(forceforward, ForceMode.Acceleration);
+                if (transform.rotation.x < 0.25f)
+                {
+                    transform.Rotate(Vector3.right * 45 * Time.deltaTime);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rb.AddForce(forceback, ForceMode.Acceleration);
+                if (transform.rotation.x > -0.25f)
+                {
+                    transform.Rotate(Vector3.left * 45 * Time.deltaTime);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.AddForce(forceleft, ForceMode.Acceleration);
+                if (transform.rotation.z < 0.25f)
+                {
+                    transform.Rotate(Vector3.forward * 45 * Time.deltaTime);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.AddForce(forceright, ForceMode.Acceleration);
+                if (transform.rotation.z > -0.25f)
+                {
+                    transform.Rotate(Vector3.back * 45 * Time.deltaTime);
+                }
+            }
+
+            //slowly normalize rotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime);
+
+            //gravity (usegravity was working weird so I just added it myself)
+            rb.AddForce(forcedown, ForceMode.Acceleration);
         }
     }
     
@@ -49,7 +110,7 @@ public class PlaneControl : MonoBehaviour
             if (Input.GetKey(KeyCode.W))
             {
                 //rb.transform.Translate(Vector3.forward * accelerationSpeed);
-                rb.velocity += Vector3.forward * accelerationSpeed;
+                //rb.AddForce(forceup * 3, ForceMode.Acceleration);
                 //if (transform.rotation.eulerAngles.z > 180)
                 //{
                 //    rb.transform.Rotate(Vector3.right * (Mathf.Sin((-270 + transform.rotation.eulerAngles.z) * Mathf.PI / 180)));
@@ -61,7 +122,7 @@ public class PlaneControl : MonoBehaviour
 
             if (Input.GetKey(KeyCode.S))
             {
-                rb.transform.Translate(Vector3.back * accelerationSpeed);
+                //rb.transform.Translate(Vector3.back * accelerationSpeed);
             }
 
             if (Input.GetKey(KeyCode.A))
@@ -72,40 +133,20 @@ public class PlaneControl : MonoBehaviour
                 //    Debug.Log("z value is: " + transform.rotation.eulerAngles.z);
                 //    transform.Rotate(Vector3.forward * 45 * Time.deltaTime);
                 //}
-                rb.AddForce(Vector3.left * accelerationSpeed, ForceMode.Impulse);
+                // rb.AddForce(Vector3.left * accelerationSpeed, ForceMode.Impulse);
 
             }
 
             if (Input.GetKey(KeyCode.D))
             {
                 //transform.Rotate(Vector3.up * 45 * Time.deltaTime);
-                if (transform.rotation.z > -0.2f)
-                {
-                    Debug.Log("z value is: " + transform.rotation.eulerAngles.z);
-                    transform.Rotate(Vector3.back * 45 * Time.deltaTime);
-                }
+                //if (transform.rotation.z > -0.2f)
+                //{
+                //    Debug.Log("z value is: " + transform.rotation.eulerAngles.z);
+                //    transform.Rotate(Vector3.back * 45 * Time.deltaTime);
+                //}
             }
-
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Rotate(Vector3.left * 45 * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Rotate(Vector3.right * 45 * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Rotate(Vector3.forward * 45 * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Rotate(Vector3.back * 45 * Time.deltaTime);
-            }
-
+            
             translateRotateScale();
             translateForward();
             dropWater();
